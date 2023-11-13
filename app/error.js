56b538c7126ec46b5494569969a6e6ac00dcf53a
saifil <saifilmomin4@gmail.com>
2023-11-13 11:52:24 -0500

    added button to error page

--- app/error.js
+++ app/error.js
@@ -1,16 +1,28 @@
-'use client'
+'use client';
+import { useRouter } from "next/navigation";
 export default function Error({ error, reset }) {
+
+    const router = useRouter();
+
+    const goHome = () => {
+        router.push('/');
+    }
+
     return (
         <main>
             <div>
-                <h2>Something went wrong!</h2>
+                <h1>Something went wrong!</h1>
                 <button
+                    className="button"
                     onClick={
-                        // Attempt to recover by trying to re-render the segment
                         () => reset()
                     }
                 >
                     Try again
+                </button>
+                <br />
+                <button className="button" onClick={goHome}>
+                    Go to home
                 </button>
             </div>
         </main>
